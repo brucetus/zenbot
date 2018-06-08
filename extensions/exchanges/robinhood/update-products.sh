cc2fb517417e9806ccf347dd58ac2e787c53d34c
brucetus <31704711+brucetus@users.noreply.github.com>
2018-06-07 20:03:08 -0700

    Update update-products.sh

--- extensions/exchanges/robinhood/update-products.sh
+++ extensions/exchanges/robinhood/update-products.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env node
-let robinhood = require('robinhood')
+let Robinhood = require('robinhood')
 
-  curl -v https://api.robinhood.com/instruments/ \
-  -H "Accept: application/json"
-  if (err) {
-    console.error(err);
-  } else {
-    var products = []
+  Robinhood.instruments(function(err, response, body){
+    if (err) {
+      console.error(err);
+    } else {
+      var products = []
 
-    markets.forEach(function (market) {
+      markets.forEach(function (market) {
 
-      products.push({
-        id: market.symbol,
-        asset: market.symbol,
-        currency: 'USD',
-        min_size: 0,
-        max_size: 99999999,
-        increment: market.min_tick_size,
-        asset_increment: 0,
-        label: market.symbol + '/' + 'USD'
+        products.push({
+          id: market.symbol,
+          asset: market.symbol,
+          currency: 'USD',
+          min_size: 0,
+          max_size: 99999999,
+          increment: market.min_tick_size,
+          asset_increment: 0,
+          label: market.symbol + '/' + 'USD'
+        })
+
       })
 
-    })
-
-    var target = require('path').resolve(__dirname, 'products.json')
-    require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
-    console.log('wrote', target)
-    process.exit()
-  }
+      var target = require('path').resolve(__dirname, 'products.json')
+      require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
+      console.log('wrote', target)
+      process.exit()
+    }
+  })
+})
