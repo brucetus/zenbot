6a7650b41c32a6b2761002fe6189e56290542c90
brucetus <31704711+brucetus@users.noreply.github.com>
2018-09-28 08:38:02 -0700

    Update update-products.sh

--- extensions/exchanges/krakenccxt/update-products.sh
+++ extensions/exchanges/krakenccxt/update-products.sh
@@ -1,104 +1,48 @@
 #!/usr/bin/env node
+let ccxt = require('ccxt')
 
-var KrakenClient = require('kraken-api')
-var kraken = new KrakenClient()
+new ccxt.kraken().fetch_markets().then(function(markets) {
+  var products = []
 
-var mapping
-var products = []
+  markets.forEach(function (market) {
+    var currStepSize = market.info.filters[0].tickSize
+    for (i = currStepSize.length - 1; i > 0; i--) {
+      if (currStepSize[i] === '0')
+        currStepSize = currStepSize.slice(0, i)
+      else
+        break;
+    }
 
-function addProduct(base, quote, altname) {
-  var min_size = '0.01'
-  switch (base) {
-  case 'XREP':
-    min_size = '0.3'
-    break;
-  case 'XBT':
-    min_size = '0.002'
-    break;
-  case 'BCH':
-    min_size = '0.002'
-    break;
-  case 'DASH':
-    min_size = '0.03'
-    break;
-  case 'EOS':
-    min_size = '3.0'
-    break;
-  case 'XETH':
-    min_size = '0.02'
-    break;
-  case 'XETC':
-    min_size = '0.3'
-    break;
-  case 'GNO':
-    min_size = '0.03'
-    break;
-  case 'XICN':
-    min_size = '2.0'
-    break;
-  case 'XLTC':
-    min_size = '0.1'
-    break;
-  case 'XMLN':
-    min_size = '0.1'
-    break;
-  case 'XLTC':
-    min_size = '0.1'
-    break;
-  case 'XXMR':
-    min_size = '0.1'
-    break;
-  case 'XXRP':
-    min_size = '30'
-    break;
-  case 'XXLM':
-    min_size = '300'
-    break;
-  case 'XZEC':
-    min_size = '0.03'
-    break;
-  case 'USDT':
-    min_size = '5'
-    break;
-  default:
-    break;
-  }
-  products.push({
-    asset: base,
-    currency: quote,
-    min_size: min_size,
-    increment: '0.01',
-    label: getPair(base) + '/' + getPair(quote)
-  })
-}
+    var assetStepSize = market.info.filters[1].stepSize
+    for (i = assetStepSize.length - 1; i > 0; i--) {
+      if (assetStepSize[i] === '0')
+        assetStepSize = assetStepSize.slice(0, i)
+      else
+        break
+    }
 
-function getPair(name) {
-  return mapping[name].altname
-}
+    var min_size    = Number(market.info.filters[1].minQty);
+    var minNotional = Number(market.info.filters[2].minNotional);
+    // Orders must be strictly greater than minNotional
+    if (min_size <= minNotional) {
+      min_size += Number(assetStepSize);
+    }
+    min_size = min_size.toString();
 
-kraken.api('Assets', null, function(error, data) {
-  if (error) {
-    console.log(error)
-    process.exit(1)
-  } else {
-
-    mapping = data.result
-
-    kraken.api('AssetPairs', null, function(error, data) {
-      if (error) {
-        console.log(error)
-        process.exit(1)
-      } else {
-        Object.keys(data.result).forEach(function(result) {
-          if (!result.match('\.d')) {
-            addProduct(data.result[result].base, data.result[result].quote, data.result[result].altname)
-          }
-        })
-        var target = require('path').resolve(__dirname, 'products.json')
-        require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
-        console.log('wrote', target)
-        process.exit()
-      }
+    products.push({
+      id: market.id,
+      asset: market.base,
+      currency: market.quote,
+      min_size: min_size,
+      max_size: '100000',
+      increment: currStepSize,
+      asset_increment: assetStepSize,
+      label: market.base + '/' + market.quote
     })
-  }
+  })
+
+  var target = require('path').resolve(__dirname, 'products.json')
+  require('fs').writeFileSync(target, JSON.stringify(products, null, 2))
+  console.log('wrote', target)
+  process.exit()
 })
