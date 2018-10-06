b8e844cfa5c279e58f3509fdd55c66253af88382
brucetus <31704711+brucetus@users.noreply.github.com>
2018-10-05 17:05:38 -0700

    Update exchange.js

--- extensions/exchanges/krakenccxt/exchange.js
+++ extensions/exchanges/krakenccxt/exchange.js
@@ -176,10 +176,10 @@ module.exports = function kraken (conf) {
       opts.side = 'buy'
       delete opts.order_type
       if (so.leverage > 1) {
-        opts.leverage = so.leverage
+        args.leverage = so.leverage
       }
       var order = {}
-      client.createOrder(joinProduct(opts.product_id), opts.type, opts.side, this.roundToNearest(opts.size, opts), opts.price, opts.leverage).then(result => {
+      client.createOrder(joinProduct(opts.product_id), opts.type, opts.side, this.roundToNearest(opts.size, opts), opts.price, args.leverage).then(result => {
         if (result && result.message === 'Insufficient funds') {
           order = {
             status: 'rejected',
@@ -238,7 +238,7 @@ module.exports = function kraken (conf) {
         args.leverage = so.leverage
       }
       var order = {}
-      client.createOrder(joinProduct(opts.product_id), opts.type, opts.side, this.roundToNearest(opts.size, opts), opts.price, { leverage: args.leverage }).then(result => {
+      client.createOrder(joinProduct(opts.product_id), opts.type, opts.side, this.roundToNearest(opts.size, opts), opts.price, args.leverage).then(result => {
         if (result && result.message === 'Insufficient funds') {
           order = {
             status: 'rejected',
