e5fff6288c8125aee064e2be9194cb9deab831f4
brucetus <31704711+brucetus@users.noreply.github.com>
2018-10-06 10:58:05 -0700

    Update exchange.js

--- extensions/exchanges/krakenccxt/exchange.js
+++ extensions/exchanges/krakenccxt/exchange.js
@@ -174,7 +174,6 @@ module.exports = function kraken (conf) {
         console.log('\nFunction: trade')
         console.log(params)
       }
-      await client.loadMarkets ();
       client.verbose = true;  // ←-- add this and post your verbose request/response without your keys here
       var order = {}
       client.createOrder(joinProduct(opts.product_id), opts.type, opts.side, this.roundToNearest(opts.size, opts), opts.price, { 'leverage': args.leverage }).then(result => {
