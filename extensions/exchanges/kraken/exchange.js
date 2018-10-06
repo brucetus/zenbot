4dc17210dec55f5ca8be02c53acd3651dd1fa35f
brucetus <31704711+brucetus@users.noreply.github.com>
2018-10-06 14:21:19 -0700

    Update exchange.js

--- extensions/exchanges/kraken/exchange.js
+++ extensions/exchanges/kraken/exchange.js
@@ -326,9 +326,7 @@ module.exports = function container(conf) {
       }
       client.api('QueryOrders', params, function(error, data) {
         if (error) {
-          if (error.message.match(recoverableErrors)) {
-            return retry('getOrder', args, error)
-          }
+          return retry('getOrder', args, error)
           console.error(('\ngetOrder error:').red)
           console.error(error)
           return cb(error)
