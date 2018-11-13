f69c8f38b51ceca5715e9ee0948643d7455f5725
brucetus <31704711+brucetus@users.noreply.github.com>
2018-11-13 12:23:07 -0800

    Update strategy.js

--- extensions/strategies/srsi/strategy.js
+++ extensions/strategies/srsi/strategy.js
@@ -4,7 +4,7 @@ var z = require('zero-fill')
 
 module.exports = {
   name: 'srsi',
-  description: 'Creates pyramid orders using SRSI.',
+  description: 'Buys and sells when SRSI K crosses above or below threshold.',
 
   getOptions: function () {
     this.option('period_length', 'period length', String, '2h')
@@ -14,40 +14,32 @@ module.exports = {
     this.option('srsi_periods', 'number of srsi periods', 14)
     this.option('srsi_k', '%D line', Number, 3)
     this.option('srsi_d', '%D line', Number, 3)
-    this.option('oversold_srsi', 'buy when srsi reaches or drops below this value', Number, 5)
-    this.option('overbought_srsi', 'sell when srsi reaches or goes above this value', Number, 95)
+    this.option('oversold_srsi', 'buy when srsi reaches or drops below this value', Number, 20)
+    this.option('overbought_srsi', 'sell when srsi reaches or goes above this value', Number, 80)
     this.option('srsi_recover', 'allow srsi to recover this many points before buying', Number, 1)
   },
 
   calculate: function (s) {
     srsi(s, 'srsi', s.options.srsi_periods, s.options.srsi_k, s.options.srsi_d)
     if (typeof s.period.srsi_K === 'number') {
-      if (s.period.srsi_K <= s.options.oversold_srsi) {
-        s.srsi_low = s.period.srsi_K
-        s.trend = 'oversold'
-      }
-      if (s.period.srsi_K >= s.options.overbought_srsi) {
-        s.srsi_high = s.period.srsi_K
-        s.trend = 'overbought'
-      }
       if (s.options.buy !== false) {
-        if (s.trend == 'oversold') {
-          s.srsi_low = Math.min(s.srsi_low, s.period.srsi_K)
-          if (s.period.srsi_K >= s.srsi_low + s.options.srsi_recover) {
-            s.trend = 'long'
-            s.signal = 'buy'
-            s.srsi_high = s.period.srsi_K
+        if (s.period.srsi_K > s.options.oversold_srsi && s.lookback[0].srsi_K < s.options.oversold_srsi) {
+          if (s.trend !== 'up') {
+            s.acted_on_trend = false
           }
+          s.trend = 'up'
+          if (dupOrderWorkAround.checkForPriorBuy(s))
+          s.signal = !s.acted_on_trend ? 'buy' : null
         }
       }
       if (s.options.sell !== false) {
-        if (s.trend == 'overbought') {
-          s.srsi_high = Math.max(s.srsi_high, s.period.srsi_K)
-          if (s.period.srsi_K <= s.srsi_high - s.options.srsi_recover) {
-            s.trend = 'short'
-            s.signal = 'sell'
-            s.srsi_low = s.period.srsi_K
+        if (s.period.srsi_K < s.options.overbought_srsi && s.lookback[0].srsi_K > s.options.overbought_srsi) {
+          if (s.trend !== 'down') {
+            s.acted_on_trend = false
           }
+          s.trend = 'down'
+          if (dupOrderWorkAround.checkForPriorSell(s))
+          s.signal = !s.acted_on_trend ? 'sell' : null
         }
       }
     }
@@ -62,7 +54,7 @@ module.exports = {
     var cols = []
     if (typeof s.period.srsi_K == 'number') {
       var color = 'grey'
-      cols.push(z(4, n(s.period.srsi_K).format('0'), ' ')[color])
+      cols.push(z(8, n(s.period.srsi_K).format('0'), ' ')[color])
     }
     return cols
   }
