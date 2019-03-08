8810adf774037ed16ea981876b13e15431acd186
brucetus <31704711+brucetus@users.noreply.github.com>
2019-03-08 15:29:27 -0800

    Update exchange.js

--- extensions/exchanges/krakenb/exchange.js
+++ extensions/exchanges/krakenb/exchange.js
@@ -14,7 +14,7 @@ module.exports = function container(conf) {
 
   var public_client, authed_client, binance_client
   let firstRun = false
-  let allowGetMarketCall = true
+  let allowGetMarketCall = false
   // var recoverableErrors = new RegExp(/(ESOCKETTIMEDOUT|ETIMEDOUT|ECONNRESET|ECONNREFUSED|ENOTFOUND|API:Invalid nonce|API:Rate limit exceeded|between Cloudflare and the origin web server)/)
   var recoverableErrors = new RegExp(/(ESOCKETTIMEDOUT|ETIMEDOUT|ECONNRESET|ECONNREFUSED|ENOTFOUND|API:Invalid nonce|between Cloudflare and the origin web server|The web server reported a gateway time-out|The web server reported a bad gateway|525: SSL handshake failed|Service:Unavailable|api.kraken.com \| 522:)/)
   var silencedRecoverableErrors = new RegExp(/(ESOCKETTIMEDOUT|ETIMEDOUT)/)
