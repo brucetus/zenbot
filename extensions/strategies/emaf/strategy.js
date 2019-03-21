4cb8711dad13d826e847db898ea98ca2386f6a8a
brucetus <31704711+brucetus@users.noreply.github.com>
2019-03-21 12:19:14 -0700

    Update strategy.js

--- extensions/strategies/emaf/strategy.js
+++ extensions/strategies/emaf/strategy.js
@@ -68,13 +68,3 @@ module.exports = {
     return cols
   }
 }
-
-function round(n, digits) {
-  if (digits === undefined) {
-    digits = 0
-  }
-  var multiplicator = Math.pow(10, digits)
-  n = parseFloat((n * multiplicator).toFixed(11))
-  var test =(Math.round(n) / multiplicator)
-  return +(test.toFixed(digits))
-}
