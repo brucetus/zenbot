0773cedfd65be8072f9b6f70644c6fe70a790bdd
brucetus <31704711+brucetus@users.noreply.github.com>
2019-04-03 17:24:37 -0700

    Update update-products.sh

--- extensions/exchanges/kraken/update-products.sh
+++ extensions/exchanges/kraken/update-products.sh
@@ -50,8 +50,8 @@ function addProduct(base, quote, altname) {
     min_size = '0.1'
     break;
   case 'XXRP':
-    min_size = '30',
-    increment = '0.0001"
+    min_size = '30'
+    increment = '0.0001'
     break;
   case 'XXLM':
     min_size = '300'
