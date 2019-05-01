8163eb55fe549294c5c857b86ae9701a4ce3f03e
brucetus <31704711+brucetus@users.noreply.github.com>
2019-05-01 16:05:25 -0700

    Update exchange.js

--- extensions/exchanges/krakencb/exchange.js
+++ extensions/exchanges/krakencb/exchange.js
@@ -123,7 +123,7 @@ module.exports = function container(conf) {
       if (opts.product_id == 'XXRP-ZUSD') opts.product_id = 'XRP/USD'
       if (opts.product_id == 'BCH-ZUSD') opts.product_id = 'BCH/USD'
       const symbol = opts.product_id
-      client.fetchTrades(symbol, args).then(result => {
+      client.fetchTrades(symbol, opts.from, args).then(result => {
         var trades = result.map(trade => ({
           trade_id: trade.id,
           time: trade.timestamp,
