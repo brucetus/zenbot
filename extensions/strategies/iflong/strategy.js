773ee4f3480499a5ed61e7144e28ebf9b996488a
brucetus <31704711+brucetus@users.noreply.github.com>
2019-05-16 17:26:32 -0700

    Add close option

--- extensions/strategies/iflong/strategy.js
+++ extensions/strategies/iflong/strategy.js
@@ -11,6 +11,7 @@ module.exports = {
   getOptions: function () {
     this.option('period_length', 'period length', String, '2h')
     this.option('min_periods', 'min periods', Number, 120)
+    this.option('close', 'close', Boolean, false)
     this.option('buy', 'buy', Boolean, false)
     this.option('sell', 'sell', Boolean, false)
     this.option('up', 'up', Number, 1.01)
@@ -23,24 +24,26 @@ module.exports = {
 
   calculate: function (s) {
     if (s.lookback[s.options.min_periods]) {
-      if (s.options.buy !== false) {
-        if ((s.period.high / s.upfractal > s.options.up) && (s.period.high / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) > s.options.up)) {
-          if (s.trend !== 'up') {
-            s.acted_on_trend = false
+      if (s.options.close == false) {
+        if (s.options.buy !== false) {
+          if ((s.period.high / s.upfractal > s.options.up) && (s.period.high / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) > s.options.up)) {
+            if (s.trend !== 'up') {
+              s.acted_on_trend = false
+            }
+            s.trend = 'up'
+            if (dupOrderWorkAround.checkForPriorBuy(s))
+            s.signal = !s.acted_on_trend ? 'buy' : null
           }
-          s.trend = 'up'
-          if (dupOrderWorkAround.checkForPriorBuy(s))
-          s.signal = !s.acted_on_trend ? 'buy' : null
         }
-      }
-      if (s.options.sell !== false) {
-        if ((s.period.low / s.downfractal < s.options.down) && (s.period.low / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) < s.options.down)) {
-          if (s.trend !== 'down') {
-            s.acted_on_trend = false
+        if (s.options.sell !== false) {
+          if ((s.period.low / s.downfractal < s.options.down) && (s.period.low / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) < s.options.down)) {
+            if (s.trend !== 'down') {
+              s.acted_on_trend = false
+            }
+            s.trend = 'down'
+            if (dupOrderWorkAround.checkForPriorSell(s))
+            s.signal = !s.acted_on_trend ? 'sell' : null
           }
-          s.trend = 'down'
-          if (dupOrderWorkAround.checkForPriorSell(s))
-          s.signal = !s.acted_on_trend ? 'sell' : null
         }
       }
     }
@@ -66,6 +69,28 @@ module.exports = {
       }
       if (s.lookback[3].low >= s.lookback[1].low && s.lookback[2].low >= s.lookback[1].low && s.lookback[0].low >= s.lookback[1].low && s.period.low >= s.lookback[1].low) {
         s.downfractal = s.lookback[1].low
+      }
+      if (s.options.close !== false) {
+        if (s.options.buy !== false) {
+          if ((s.period.high / s.upfractal > s.options.up) && (s.period.high / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) > s.options.up)) {
+            if (s.trend !== 'up') {
+              s.acted_on_trend = false
+            }
+            s.trend = 'up'
+            if (dupOrderWorkAround.checkForPriorBuy(s))
+            s.signal = !s.acted_on_trend ? 'buy' : null
+          }
+        }
+        if (s.options.sell !== false) {
+          if ((s.period.low / s.downfractal < s.options.down) && (s.period.low / Math.max(s.lookback[29].senkou_a, s.lookback[29].senkou_b) < s.options.down)) {
+            if (s.trend !== 'down') {
+              s.acted_on_trend = false
+            }
+            s.trend = 'down'
+            if (dupOrderWorkAround.checkForPriorSell(s))
+            s.signal = !s.acted_on_trend ? 'sell' : null
+          }
+        }
       }
     }
     cb()
