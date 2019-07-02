c581aa5d0d41abde35f704232bee8c6af2b1a362
brucetus <31704711+brucetus@users.noreply.github.com>
2019-07-02 12:05:57 -0700

    Update strategy.js

--- extensions/strategies/emafcmf/strategy.js
+++ extensions/strategies/emafcmf/strategy.js
@@ -21,7 +21,7 @@ module.exports = {
   calculate: function (s) {
     if (s.lookback[s.options.ema]) {
       ema(s, 'ema', s.options.ema)
-      s.cmf = ((s.period.volume * ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low)) + cmf) / (s.period.volume + cmf)
+      s.period.cmf = ((s.period.volume * ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low)) + cmf) / (s.period.volume + cmf)
       if (s.options.close == false) {
         if (s.options.buy !== false) {
           if (s.period.high > s.upfractal && s.period.high > s.period.ema && s.period.cmf > 0) {
