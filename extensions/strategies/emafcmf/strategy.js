948c3f54059cad2442e08c4bd0ca306001801cdc
brucetus <31704711+brucetus@users.noreply.github.com>
2019-07-02 12:04:17 -0700

    Update strategy.js

--- extensions/strategies/emafcmf/strategy.js
+++ extensions/strategies/emafcmf/strategy.js
@@ -21,8 +21,7 @@ module.exports = {
   calculate: function (s) {
     if (s.lookback[s.options.ema]) {
       ema(s, 'ema', s.options.ema)
-      let mfv = s.period.volume * ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low)
-      s.cmf = (s.period.volume * ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low) + cmf) / (s.period.volume + cmf)
+      s.cmf = ((s.period.volume * ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low)) + cmf) / (s.period.volume + cmf)
       if (s.options.close == false) {
         if (s.options.buy !== false) {
           if (s.period.high > s.upfractal && s.period.high > s.period.ema && s.period.cmf > 0) {
