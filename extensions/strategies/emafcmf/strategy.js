83e16fcfe8ff1ddd3e9e06793106b3c5a192ef15
brucetus <31704711+brucetus@users.noreply.github.com>
2019-07-02 12:54:40 -0700

    Update strategy.js

--- extensions/strategies/emafcmf/strategy.js
+++ extensions/strategies/emafcmf/strategy.js
@@ -21,7 +21,6 @@ module.exports = {
   calculate: function (s) {
     if (s.lookback[s.options.ema]) {
       ema(s, 'ema', s.options.ema)
-      s.mfv = ((s.period.close - s.period.low) - (s.period.high - s.period.close)) / (s.period.high - s.period.low)
       cmf(s, 'cmf', s.options.cmf)
       if (s.options.close == false) {
         if (s.options.buy !== false) {
