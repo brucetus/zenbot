6d04a8c6d563f3880fb2d9a3584b43997708ca46
brucetus <31704711+brucetus@users.noreply.github.com>
2019-07-13 10:24:52 -0700

    Update exchange.js

--- extensions/exchanges/krakenccxt/exchange.js
+++ extensions/exchanges/krakenccxt/exchange.js
@@ -98,7 +98,7 @@ module.exports = function kraken (conf) {
       //   })
       // }
       //else {
-        client.fetchTrades(joinProduct(opts.product_id), args.since).then(result => {
+        client.fetchTrades(joinProduct(opts.product_id), undefined, undefined, args.since).then(result => {
           var trades = result.map(function (trade) {
             return {
               trade_id: trade[2] + trade[1] + trade[0],
