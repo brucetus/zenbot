1bf3343fa6c5b26e3924db39ca68f15649f44bb8
brucetus <31704711+brucetus@users.noreply.github.com>
2019-07-13 10:22:40 -0700

    Update exchange.js

--- extensions/exchanges/krakenccxt/exchange.js
+++ extensions/exchanges/krakenccxt/exchange.js
@@ -68,6 +68,7 @@ module.exports = function kraken (conf) {
         interval: 120,
         since: Number(opts.from) * 1000000
       }
+      console.log(args.since)
       if (allowGetMarketCall != true) {
         cb(null, [])
         return null
